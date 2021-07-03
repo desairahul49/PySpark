428abf37424019668fa6dcdae7f06bd10a915218
Rahul Desai <39995416+desairahul49@users.noreply.github.com>
2021-07-03 11:53:35 +0530

    recursing in sub paths
    added recursion logic to rename files in subpaths

--- rename.py
+++ rename.py
@@ -6,6 +6,7 @@ def file_rename_tool(name_pattern, path, ext= None):
     # check if the input path valid or not
     if os.path.exists(path):
         file_list = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i))]
+        path_list = [os.path.join(path,i) for i in os.listdir(path) if os.path.isdir(os.path.join(path,i))]
         for i in range(len(file_list)):
             if not ext:
                 ext = os.path.splitext(os.path.join(path,file_list[i]))[-1]
@@ -14,6 +15,9 @@ def file_rename_tool(name_pattern, path, ext= None):
                 os.rename(os.path.join(path,file_list[i]), os.path.join(path,name_pattern+ str.zfill(str(i+1),3) +'.'+ ext))
     else:
         print(f'Invalid path provied {path}')
+    #recursing in sub paths
+    for path in path_list:
+        file_rename_tool(name_pattern, path, ext)
 
 
 if __name__ == "__main__":
